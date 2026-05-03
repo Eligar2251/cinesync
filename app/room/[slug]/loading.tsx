567162ce7ece9e5afd4aa4cea76c0e31e66b1248
Eligar2251 <eligar225@yandex.ru>
2026-05-03 21:55:59 +0700

    Fix for CineSync

---
+++ app/room/[slug]/loading.tsx
@@ -0,0 +1,6 @@
+// app/room/[slug]/loading.tsx
+import { RoomLayoutSkeleton } from '@/components/UI/Skeleton/RoomSkeleton'
+
+export default function Loading() {
+  return <RoomLayoutSkeleton />;
+}
---
+++ app/room/create/loading.tsx
@@ -0,0 +1,4 @@
+// app/room/create/loading.tsx
+export default function Loading() {
+  return null; // или любой скелетон
+}
